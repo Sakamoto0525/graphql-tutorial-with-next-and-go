fe465424bd6c04fd10ccd3b0f596bd830386142c
Sakamoto0525 <525masaya@gmail.com>
2022-05-18 21:38:43 +0900

    goコード追加

--- api/graph/schema.resolvers.go
+++ api/graph/schema.resolvers.go
@@ -6,25 +6,62 @@ package graph
 import (
 	"context"
 	"fmt"
+	"sort"
+	"strconv"
+	"time"
 
 	"github.com/Sakamoto0525/api/graph/generated"
 	"github.com/Sakamoto0525/api/graph/model"
 )
 
+var posts []*model.Post = make([]*model.Post, 0)
+
 func (r *mutationResolver) CreatePost(ctx context.Context, title string, url string) (*model.Post, error) {
-	panic(fmt.Errorf("not implemented"))
+	post := model.Post{
+		ID:        fmt.Sprintf("%d", len(posts)+1),
+		Title:     title,
+		URL:       url,
+		Votes:     0,
+		CreatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	posts = append(posts, &post)
+
+	return &post, nil
 }
 
 func (r *mutationResolver) UpdatePost(ctx context.Context, id string, votes *int) (*model.Post, error) {
-	panic(fmt.Errorf("not implemented"))
+	if votes == nil {
+		return nil, nil
+	}
+	i, _ := strconv.Atoi(id)
+	posts[i-1].Votes = *votes
+
+	return posts[i-1], nil
 }
 
 func (r *queryResolver) AllPosts(ctx context.Context, orderBy *model.OrderBy, first int, skip int) ([]*model.Post, error) {
-	panic(fmt.Errorf("not implemented"))
+	if skip > len(posts) {
+		skip = len(posts)
+	}
+	if (skip + first) > len(posts) {
+		first = len(posts) - skip
+	}
+	sortedPosts := make([]*model.Post, len(posts))
+	copy(sortedPosts, posts)
+	if orderBy != nil && *orderBy == "createdAt_DESC" {
+		sort.SliceStable(sortedPosts, func(i, j int) bool {
+			return sortedPosts[i].CreatedAt > sortedPosts[j].CreatedAt
+		})
+	}
+	slicePosts := sortedPosts[skip : skip+first]
+
+	return slicePosts, nil
 }
 
 func (r *queryResolver) AllPostsMeta(ctx context.Context) (*model.PostsMeta, error) {
-	panic(fmt.Errorf("not implemented"))
+	postsMeta := model.PostsMeta{Count: len(posts)}
+
+	return &postsMeta, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
